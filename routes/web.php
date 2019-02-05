4e3f36cad50522ad28254bee3694a1f147020bbe
Martin Juul <martin@juul.xyz>
2019-02-05 18:55:37 +0100

    Remove stale files & player

--- routes/web.php
+++ routes/web.php
@@ -11,27 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-
-});
-
-Route::get('player/{type}/{slug}', 'PlayerController@show')->name('player');
-
 Route::get('stream/{path}', 'StreamController@show')
     ->name('stream');
 
 Route::get('stream/playlist/{path}', 'StreamController@getPlaylist')
     ->name('stream-playlist');
-
-Route::get('/movies', function () {
-    $movies = \App\Models\Movie::all();
-
-    $movies->each(function (\App\Models\Movie $movie) {
-        echo '<a href="' . route('player', [
-                'type' => 'movie', 'slug' => $movie->slug,
-            ]) . '"><img style="width: 100px;" src="/storage/' . $movie->poster . '" /></a>';
-        dump($movie->toArray());
-    });
-});
 
 Route::get('/storage/transcode/{path}', 'StreamController@getSegment');
